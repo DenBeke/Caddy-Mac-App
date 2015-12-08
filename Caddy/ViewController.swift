74dd62a412d8872bb7055afde2272a9f5310ad6d
MathiasB <git@denbeke.be>
2015-12-08 21:46:39 +0100

    Initial Commit

---
+++ Caddy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Caddy
+//
+//  Created by Mathias Beke on 8/12/15.
+//  Copyright © 2015 DenBeke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
