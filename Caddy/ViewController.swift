bc61d96804fd3b820fe06d0535a514f6fbcc789a
MathiasB <git@denbeke.be>
2015-12-09 16:53:47 +0100

    Launching Caddy as NSTask

--- Caddy/ViewController.swift
+++ Caddy/ViewController.swift
@@ -8,12 +8,76 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+func executeCommand(command: String, args: [String]) -> String {
+    
+    let task = NSTask()
+    
+    task.launchPath = command
+    task.arguments = args
+    
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output: String = String(data: data, encoding: NSUTF8StringEncoding)!
+    
+    return output
+}
 
+
+class ViewController: NSViewController {
+    
+    var task = NSTask()
+    var pipe = NSPipe()
+    
+    var path    = ""
+    var running = false;
+    
+    @IBOutlet weak var start_stop_button: NSButton!
+    
     override func viewDidLoad() {
+        
+        // Delegate
+        //window.delegate = self
+        
+        // Set path to executable
+        path = String(NSBundle.mainBundle().URLForResource("caddy", withExtension: "")!)
+        path = path.stringByReplacingOccurrencesOfString(
+            "file://",
+            withString: "",
+            options: .RegularExpressionSearch)
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleServer(sender: AnyObject) {
+        if !running {
+            startCaddy()
+        } else {
+            stopCaddy()
+        }
+    }
+    
+    func startCaddy() {
+        running = true
+        start_stop_button.title = "Stop Caddy"
+        self.task = NSTask()
+        self.pipe = NSPipe()
+        self.task.launchPath = path
+        self.task.standardOutput = pipe
+        self.task.arguments = []
+        self.task.launch()
+        print("Launched Caddy")
+    }
+    
+    func stopCaddy() {
+        running = false
+        start_stop_button.title = "Start Caddy"
+        task.terminate()
+        print("Terminated Caddy")
     }
 
     override var representedObject: AnyObject? {
